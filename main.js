c2c717430e00496653c5a82bbfb116202097fdf8
Keith Price <keithprice.dev@gmail.com>
2022-02-09 17:16:46 +0700

    late init, already added Earth and moon to canvas. Set rough orbits and rotation for both. Used 3dobject (centered inside Earth) to control the mooon's orbit

---
+++ main.js
@@ -0,0 +1,105 @@
+import './style.css';
+
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+	canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(40);
+
+// Earth
+const earthTexture = new THREE.TextureLoader().load(
+	'./textures/2k_earth_daymap.jpg'
+);
+
+const earthGeometry = new THREE.SphereGeometry(10, 64, 64);
+const earthMaterial = new THREE.MeshStandardMaterial({
+	map: earthTexture,
+});
+
+const earth = new THREE.Mesh(earthGeometry, earthMaterial);
+
+scene.add(earth);
+
+// Moon
+const moonTexture = new THREE.TextureLoader().load('./textures/2k_moon.jpg');
+
+const moonGeometry = new THREE.SphereGeometry(2.7, 64, 64);
+const moonMaterial = new THREE.MeshStandardMaterial({
+	map: moonTexture,
+});
+
+const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+moon.position.set(45, 0, 0);
+
+scene.add(moon);
+
+// Object to control moon orbit
+const moonOrbitCenter = new THREE.Object3D();
+scene.add(moonOrbitCenter);
+
+// lighting
+const pointLight = new THREE.PointLight(0xffffff, 1.75);
+// const ambientLight = new THREE.AmbientLight(0xffffff);
+pointLight.position.set(500, 5, 300);
+
+scene.add(pointLight);
+
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// // const gridHelper = new THREE.GridHelper(200, 50);
+
+// scene.add(lightHelper);
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+// stars
+function addStar() {
+	const starGeometry = new THREE.SphereGeometry(0.25, 24, 25);
+	const starMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	const star = new THREE.Mesh(starGeometry, starMaterial);
+
+	const [x, y, z] = Array(3)
+		.fill()
+		.map(() => THREE.MathUtils.randFloatSpread(1000));
+	star.position.set(x, y, z);
+	scene.add(star);
+}
+
+// background
+// const spaceBackground = new THREE.TextureLoader().load(
+// 	'/textures/photo-1506318137071-a8e063b4bec0.jfif'
+// );
+// scene.background = spaceBackground;
+
+Array(200).fill().forEach(addStar);
+
+// animation
+function animate() {
+	requestAnimationFrame(animate);
+	// sphere.rotation.x += 0.01;
+	earth.rotation.y += 0.001;
+	// not sure of orbit time, need to calculate more accurately
+	moonOrbitCenter.rotation.y += 0.000037037037;
+	moonOrbitCenter.add(moon);
+	moon.rotation.y += 0.000378;
+
+	// controls.update();
+
+	renderer.render(scene, camera);
+}
+
+animate();
